9716a295cf07ec405f5e0fef0e09c061cab3c4d4
Kitso Mothobi <kitsomothobi@gmail.com>
2026-02-22 12:52:18 +0200

    Added Jinja2 homepage templating

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Telling our program where to find templates, aka, our html files
+templates = Jinja2Templates(directory="templates")
+# From here on out, all our methods will require the request object as an input parameter
+
+
 fruits = [
     {
     "id": 1 ,
@@ -27,8 +32,11 @@ fruits = [
 
 # Our routes currently return json data, in order to turn it into HTML, we need to import HTMLResponse
 @app.get("/")
-def greetings():
-    return fruits
+def greetings(request: Request):
+    # This says, look in the templates object, and return for us, the home.html file. as a response
+        # Additionally, templateRespose takes in an optional 3rd parameter, called the context parameter, this is usally where
+        # We will be inserting our json data, in order to have it accessible to the aforementioned html page
+    return templates.TemplateResponse(request, "home.html", {"fruits": fruits, "title": "Home"})
 
 
 @app.get("/fruit")
