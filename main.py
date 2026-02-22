957a99c25119e182b3d04103a4eef3d7dcf98ede
Kitso Mothobi <kitsomothobi@gmail.com>
2026-02-22 18:31:31 +0200

    Added a functional navigation bar and homepage

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 # Templates are where we are going to write our html data in oder to make things cleaner and easier to process
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Telling our program where to find templates, aka, our html files
 templates = Jinja2Templates(directory="templates")
@@ -32,7 +34,7 @@ fruits = [
 
 # Our routes currently return json data, in order to turn it into HTML, we need to import HTMLResponse
 @app.get("/")
-def greetings(request: Request):
+def home(request: Request):
     # This says, look in the templates object, and return for us, the home.html file. as a response
         # Additionally, templateRespose takes in an optional 3rd parameter, called the context parameter, this is usally where
         # We will be inserting our json data, in order to have it accessible to the aforementioned html page
@@ -42,3 +44,11 @@ def greetings(request: Request):
 @app.get("/fruit")
 def fruit():
     return fruits
+
+@app.get("/fruit/{fruit_id}")
+def fruit(fruit_id: int):
+    for fruit in fruits:
+        if fruit["id"] == fruit_id:
+            return fruit
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found :(")
+#   Instead of returning an error message, best practice calls for us to raise an actual 404 error
